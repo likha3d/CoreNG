5296cb3c62bf634da147b1028ecf3a4fe302a564
likha3d <dev@likha3d.com>
2020-12-26 20:37:38 +0800

    Update for boards build

--- variants/same70/variant.cmake
+++ variants/same70/variant.cmake
@@ -5,12 +5,13 @@ list(APPEND SRCS
 
     "${CMAKE_CURRENT_SOURCE_DIR}/asf/common/services/clock/same70/sysclk.c"
     "${CMAKE_CURRENT_SOURCE_DIR}/asf/common/services/usb/udc/udc.c"
-    "${CMAKE_CURRENT_SOURCE_DIR}/asf/sam/components/ethernet_phy/ksz8081rna/ethernet_phy.c"
     "${CMAKE_CURRENT_SOURCE_DIR}/asf/sam/drivers/xdmac/xdmac.c"
     "${CMAKE_CURRENT_SOURCE_DIR}/asf/sam/drivers/gmac/gmac_phy.c"
     "${CMAKE_CURRENT_SOURCE_DIR}/asf/sam/drivers/rswdt/rswdt.c"
     "${CMAKE_CURRENT_SOURCE_DIR}/asf/sam/drivers/afec/afec.c"
     "${CMAKE_CURRENT_SOURCE_DIR}/asf/sam/drivers/usbhs/usbhs_device.c"
+
+    "${CMAKE_CURRENT_LIST_DIR}/DmacManager.cpp"
 )
 
 list(APPEND INCLUDE_DIRS
